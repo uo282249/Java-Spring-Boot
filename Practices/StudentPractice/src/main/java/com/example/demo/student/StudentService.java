81bc493263edd98e13e6a95fc365be33930b3667
Manuel López <uo282249@uniovi.es>
2023-02-19 21:47:26 +0100

    First version of the Student Practice

---
+++ Practices/StudentPractice/src/main/java/com/example/demo/student/StudentService.java
@@ -0,0 +1,39 @@
+package com.example.demo.student;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class StudentService {
+
+    private final StudentRepository studentRepository;
+
+    @Autowired
+    public StudentService(StudentRepository studentRepository) {
+        this.studentRepository = studentRepository;
+    }
+
+    @GetMapping
+    public List<Student> getStudents() {
+        return studentRepository.findAll();
+    }
+
+    public void addNewStudent(Student student) {
+        Optional<Student> studentByEmail = studentRepository.findStudentByEmail(student.getEmail());
+        if(studentByEmail.isPresent()) {
+            throw new IllegalStateException("email taken");
+        }
+        studentRepository.save(student);
+    }
+
+    public void deleteStudent(Long studentId) {
+
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,11 @@
-# Java-Spring-Boot
-My Spring Boot journey.
+# Spring Boot
+## Definitions
+JPA: Java Persistence API
+
+Hibernate: Most popular implementation of JPA.
+
+Spring Data JPA: Abstraction of Hibernate which makes it easy to work with applications that access the database.
+
+![image](/img/img1.jpg)
+
+![img.png](/img/img.png)
